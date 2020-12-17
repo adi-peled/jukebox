cf302782c2442749daba0dbf7c032cdedc9a737c
adi <adipeled@gmail.com>
2020-12-17 12:49:05 +0200

    added filter boxlist boxpreview

---
+++ src/cmps/BoxList/BoxList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BoxList.scss'
+import BoxPreview from '../BoxPreview/BoxPreview'
+function BoxList() {
+    return (
+        <section className="boxList">
+            this is list
+            <BoxPreview />
+        </section>
+    )
+}
+
+export default BoxList
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Filter from '../../cmps/Filter/Filter'
+import BoxList from '../../cmps/BoxList/BoxList'
 function Main() {
     return (
         <section className="main">
             main page
+            <Filter />
+            <BoxList />
         </section>
     )
 }
