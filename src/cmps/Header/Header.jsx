602ede172f05d224a518d63d443458a2a5d407f0
adi <adipeled@gmail.com>
2020-12-17 12:35:05 +0200

    header  1th

--- src/cmps/Header/Header.jsx
+++ src/cmps/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './Header.scss'
 function Header() {
     return (
-        <header className="header">
-            header
+        <header className="header flex">
+            <img className="header__logo" src="" alt="logo" />
+
+            <ul className="header__nav flex">
+                <li className="header__item">Boxes</li>
+                <li className="header__item">Create Box</li>
+                <li className="header__item">
+                    <li className="header__item">
+                        <img className="header__profile" src="" alt="profile" />
+                    </li>
+                    <ul className="profile__list"> 
+                        <li className="profile__item">Signup</li>
+                        <li className="profile__item">Login</li>
+                        <li className="profile__item">Logout</li>
+                    </ul>
+                </li>
+            </ul>
         </header>
     )
 }
