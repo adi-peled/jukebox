09e0f28b39eb04750ee75d7dfc413a1db7653b31
maorba6 <you@example.commaorba6@gmail.com>
2021-01-03 18:39:29 +0200

    sync maor and adi

--- src/cmps/BoxDetails/BoxDetails.jsx
+++ src/cmps/BoxDetails/BoxDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { youtubeService } from '../../services/youtubeService.js'
 import YouTube from 'react-youtube'
 function BoxDetails() {
     
@@ -19,21 +20,13 @@ function BoxDetails() {
 
     function showSongObj(event){
         event.preventDefault()
-        axios({
-            method: "GET",
-            url: `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=videos&q=${input}&key=AIzaSyDT9sOsVrKS57kBtrHqmY0FOgykI0fhOrY`
+        youtubeService.get(input)
+        .then(res=>{
+            setVideoId(res.items[0].id.videoId)
         })
-        .then(res => {
-             console.log(res.data);
-             setVideoId(res.data.items[0].id.videoId)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        console.log(videoId,'hi');
+      
     }
     return (
-        
         <div>
             <form action="">
                 <input placeholder="Look for a song" value={input} onChange={(event) => setInput(event.target.value)} type="search"/>
---
+++ src/services/youtubeService.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+// import he from 'he';
+const SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
+const DETAILS_URL = 'https://www.googleapis.com/youtube/v3/videos'
+
+const API_KEY = 'AIzaSyDT9sOsVrKS57kBtrHqmY0FOgykI0fhOrY';
+// const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const youtubeService = {
+    get,
+    // titleSimplify,
+    // getDuration,
+    // getSongById,
+}
+
+async function get(query) {
+    
+    try {
+        const res = await axios.get(`${SEARCH_URL}?videoCategoryId=10&part=id,snippet&videoEmbeddable=true&type=video&maxResults=10&q=${query}&key=${API_KEY}`);
+        return res.data;
+    } catch (err) {
+        console.dir(err);
+            throw (err)
+    }
+}
+
+// async function getSongById(youtubeId) {
+//     try {
+//         const res = await axios.get(`${DETAILS_URL}?id=${youtubeId}&part=id,contentDetails,snippet&key=${API_KEY}`);
+//         return res.data;
+//     } catch (err) {
+//         console.log(err);
+//         throw err;
+//     }
+// }
+
+// async function getDuration(youtubeId, timeString) {
+//     let duration
+//     if (!timeString) {
+//         try {
+//             let res = await axios.get(`${DETAILS_URL}?id=${youtubeId}&part=contentDetails&key=${API_KEY}`);
+//             duration = res.data.items[0].contentDetails.duration;
+//         } catch (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     } else duration = timeString;
+//     try {
+//         duration = duration.substring(2);
+//         duration = duration.replace('M', ':');
+//         duration = duration.split(':')
+//         duration[1] = duration[1].replace('S', '');
+//         duration[1] = duration[1].padStart(2, '0');
+//         duration = duration.join(':');
+//         return duration.toString();
+//     } catch (err) {
+//         return null;
+//     }
+// }
+
+// function titleSimplify(title) {
+//     // Removes HTML char codes
+//     let output = he.decode(title);
+//     // Removes 'Official Video' brackets
+//     const officialVideoRegex = /[([].?(official.?video)?(official music video)?.?[)\]]/ig
+//     output = output.replace(officialVideoRegex, '');
+
+//     return output;
+// }
