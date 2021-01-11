0d5541d6df7e128ad324ab6c62ac7e564f4a3449
adi <adipeled@gmail.com>
2021-01-11 21:14:34 +0200

    changed gif added mg only for try on songPreview

--- src/cmps/SocialLinks/SocialLinks.jsx
+++ src/cmps/SocialLinks/SocialLinks.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SocialLinks.scss'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import AttachFileSharpIcon from '@material-ui/icons/AttachFileSharp';
 import AddIcon from '@material-ui/icons/Add';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-function SocialLinks(props) {
+import Alert from '@material-ui/lab/Alert';
+
+function SocialLinks() {
     const url = window.location.href
+    const [isCopied, setIsCopied] = useState(false)
 
     function copyToClipboard() {
         const el = document.createElement('textarea');
@@ -18,17 +21,25 @@ function SocialLinks(props) {
         el.select();
         document.execCommand('copy');
         document.body.removeChild(el);
+
+        setIsCopied(true)
+        setTimeout(() => {
+            setIsCopied(false)
+        }, 1000)
     }
     return (
-        <div className="social-links-section flex space-between">
-            <div className="social-links-section-add">
-            <FavoriteBorderIcon/>
-            <AddIcon/>
+        <div className="social-links flex space-between">
+            <div className="social-links__add">
+                <FavoriteBorderIcon />
+                <AddIcon />
             </div>
+            {isCopied && <Alert className="social-links__success" severity="success" >
+                success!  Link copied to copy To clipboard
+                </Alert>}
             <div className="social-links-section-social">
-                <AttachFileSharpIcon onClick={copyToClipboard} /> 
-                <a href={`whatsapp://send?text=${url}`}><WhatsAppIcon/></a>
-                <a href={`https://www.facebook.com/sharer/sharer.php?href=${url}`} target="_blank"> <FacebookIcon/> </a>
+                <AttachFileSharpIcon onClick={copyToClipboard} />
+                <a href={`whatsapp://send?text=${url}`}><WhatsAppIcon /></a>
+                <a href={`https://www.facebook.com/sharer/sharer.php?href=${url}`} target="_blank"> <FacebookIcon /> </a>
             </div>
         </div>
     )
--- src/cmps/SongPreview/SongPreview.jsx
+++ src/cmps/SongPreview/SongPreview.jsx
@@ -6,36 +6,36 @@ import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-import equalizer from '../../assets/equalizer.gif'
+import equalizer from '../../assets/img/equalizer.gif'
 //redux
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
+//img
+import imgjustfornow from '../../assets/img/hero1.jpg'
 
-function SongPreview({ song, playSong, deleteSong}) {
+function SongPreview({ song, playSong, deleteSong }) {
     const [isRemoving, setIsRemoving] = useState(false)
-    const {currSong} =  useSelector(state => state.boxReducer)
+    const { currSong } = useSelector(state => state.boxReducer)
     const isSongPlaying = (song._id === currSong?._id) && currSong?.isPlaying;
 
-
-    
-    function changeIsRemoving(){
+    function changeIsRemoving() {
         setIsRemoving(!isRemoving)
-        setTimeout(()=>{
+        setTimeout(() => {
             setIsRemoving(false)
-        },3000)
+        }, 3000)
     }
 
     return (
-        <div className="flex space-between song-preview" onDoubleClick={()=>playSong(song)}>
-            <div className="song-preview-section1">
-                {isSongPlaying ?  <PauseCircleOutlineIcon className="song-preview-svg" onClick={()=>playSong(currSong)} /> : 
-                <PlayCircleOutlineIcon className="song-preview-svg" onClick={()=>playSong(song)}/>}
+        <div className="flex space-between song-preview" onDoubleClick={() => playSong(song)}>
+            <div className="song-preview-left flex">
+                {isSongPlaying ? <PauseCircleOutlineIcon className="song-preview-svg" onClick={() => playSong(currSong)} /> :
+                    <PlayCircleOutlineIcon className="song-preview-svg" onClick={() => playSong(song)} />}
+                <img className="song-preview-img" src={imgjustfornow} />
                 <div>{song.name}</div>
-
             </div>
-            <div className="song-preview-section2"> 
-                { isSongPlaying ? <img className="equalizer-gif" src={equalizer} alt=""/> : <div className="equalizer-gif"></div> }
+            <div className="song-preview-right flex">
+                {isSongPlaying ? <img className="equalizer-gif" src={equalizer} /> : <div className="equalizer-gif"></div>}
                 <div>{song.duration}</div>
-            {isRemoving ? <DeleteOutlineOutlinedIcon onClick={()=>deleteSong(song._id)} className= "song-preview-svg"/> : <MoreVertIcon onClick={changeIsRemoving} className= "song-preview-svg" />}
+                {isRemoving ? <DeleteOutlineOutlinedIcon onClick={() => deleteSong(song._id)} className="song-preview-svg" /> : <MoreVertIcon onClick={changeIsRemoving} className="song-preview-svg" />}
             </div>
         </div>
     )
