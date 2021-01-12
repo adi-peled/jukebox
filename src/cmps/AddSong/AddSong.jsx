ba87df9b0e88e84f55fd849d9c25c69bc0722c01
adi <adipeled@gmail.com>
2021-01-12 19:17:45 +0200

    add song, home render songs filter  by  genre

--- src/cmps/AddSong/AddSong.jsx
+++ src/cmps/AddSong/AddSong.jsx
@@ -4,7 +4,7 @@ import { Input } from '@material-ui/core'
 import { youtubeService } from '../../services/youtubeService';
 import CancelIcon from '@material-ui/icons/Cancel';
 
-function AddSong({ onClose ,onAddSong}) {
+function AddSong({ onClose, onAddSong }) {
     const [txt, setTxt] = useState('')
     const [songs, setSongs] = useState(null)
     const search = async (ev) => {
@@ -12,15 +12,16 @@ function AddSong({ onClose ,onAddSong}) {
         setSongs(await youtubeService.get(txt))
     }
 
-   
+
 
     return (
         <form className="addSong" onSubmit={(ev) => search(ev)} >
             <Input placeholder="Search Song" onChange={(ev) => setTxt(ev.target.value)} />
             {songs && <div className="addSong__songs">
                 {songs.map(song => {
-                    return <div key={song.id.videoId} className="addSong__song" onClick={() => onAddSong(song)}>
-                        {song.snippet.title}
+                    return <div key={song.id.videoId} className="addSong__song flex" onClick={() => onAddSong(song)}>
+                        <img className="addSong__img" src={song.snippet.thumbnails.high.url} />
+                        <span className="addSong__title"> {song.snippet.title}</span>
                     </div>
                 })}
             </div>}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import './Home.scss'
+//cmps
 import BoxList from '../../cmps/BoxList/BoxList'
 import heroImg from '../../assets/img/hero2.jpg'
 import { TweenLite } from 'gsap'
+//redux
+import { useSelector, useDispatch } from 'react-redux'
+import { loadBoxes } from '../../store/actions/boxActions'
 function Home() {
 
     const genres = ['Hip-hop', 'Electronic', 'Latin', 'Rock', 'Pop', 'Classical']
     const [myTween, setMyTween] = useState(null)
     const [myElement, setMyElement] = useState(null)
+    const { boxes } = useSelector((state) => state.boxReducer)
+    const dispatch = useDispatch()
 
 
     useEffect(() => {
         // setMyTween(TweenLite.to(myElement, 1, { x: 100, y: 100 }))
     }, [myElement])
+
+
+    useEffect(() => {
+        dispatch(loadBoxes())
+    }, [])
+
+
+
     return (
         <section className="home">
             <img ref={div => setMyElement(div)} className="home__img" src={heroImg} />
             <h2>  Top Genres</h2>
             {genres.map(genre => {
+                const filteredBoxes = boxes.filter(box => box.genre === genre)
                 return <div key={genre} className="genre">
                     <h3>{genre}</h3>
-                    <BoxList genre={genre} />
+                    <BoxList boxes={filteredBoxes} />
                 </div>
             })}
 
