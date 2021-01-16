3443c75e2eb73a5138cdefd324cc63f733039a0b
adi <adipeled@gmail.com>
2021-01-16 19:11:27 +0200

    chat css

--- src/cmps/Chat/Chat.jsx
+++ src/cmps/Chat/Chat.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import './Chat.scss'
-function Chat({box,sendMsg, isTyping}) {
+function Chat({ box, sendMsg, isTyping }) {
     const [msg, setMsg] = useState('')
-    const { currBox }= useSelector(state => state.boxReducer)
-    const  currUser = useSelector(state => state.userReducer.user)
-    
-    function handleInputChange(e){
-        isTyping(currBox,currUser)
+    const { currBox } = useSelector(state => state.boxReducer)
+    const currUser = useSelector(state => state.userReducer.user)
+
+    function handleInputChange(e) {
+        isTyping(currBox, currUser)
         setMsg(e.target.value)
     }
 
-    function handleSubmit (e){
+    function handleSubmit(e) {
         e.preventDefault()
         let message = {
             text: msg,
             from: currUser,
             toBoxId: currBox._id,
-            createdAt: new Date() 
+            createdAt: Date.now()
         }
         const data = {
             message,
@@ -26,15 +26,38 @@ function Chat({box,sendMsg, isTyping}) {
         sendMsg(data)
         setMsg('')
     }
+
+
+    function getTime(timestamp) {
+        let hour = new Date(timestamp).getHours()
+        let minutes = new Date(timestamp).getMinutes()
+        hour= hour<10? `0${hour}` : hour
+        minutes= minutes<10? `0${minutes}` : minutes
+        return `${hour}:${minutes}`
+    }
+
+
+
     return (
         <div className="chat-box flex column space-between">
-            <div>
+            <div className="chat-box__container">
                 {box && box.chat.map(msg => {
-                    return <p key={msg.createdAt}>{msg.text}</p>
+                    const { username, imgString } = msg.from
+                    const msgClass = currUser.username === username ? 'currUser ' : ''
+                    return <div className={msgClass + 'chat-box__msg  flex'} key={msg.createdAt}>
+                        <div className="chat-box__sender flex">
+                            {!msgClass && <img className="chat-box__img" src={imgString} />}
+                           {msgClass&& <span className="chat-box__username">you </span>}
+                           {!msgClass&& <span className="chat-box__username">{username} </span>}
+                            <span className="chat-box__username"> {getTime(msg.createdAt)}</span>
+                        </div>
+                        <div className="chat-box__txt">   {msg.text}</div>
+
+                    </div>
                 })}
             </div>
-            <form className="text-form" onSubmit={(e)=>handleSubmit(e)}>
-                <input type="text" onChange={(e)=>handleInputChange(e)} value={msg} placeholder="Write a message"/>
+            <form className="text-form" onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" onChange={(e) => handleInputChange(e)} value={msg} placeholder="Write a message" />
                 <button type="submit">Send</button>
             </form>
         </div>
